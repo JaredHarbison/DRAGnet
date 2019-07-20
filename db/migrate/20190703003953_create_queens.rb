c9d8489548897c50e9c4c28fa8098fe3d924cf21
github username <github email address>
2019-07-19 23:27:24 -0400

    scrape more info - hometown or birthplace, current city, date_of_birth, ethnicity

--- db/migrate/20190703003953_create_queens.rb
+++ db/migrate/20190703003953_create_queens.rb
@@ -5,6 +5,9 @@ class CreateQueens < ActiveRecord::Migration[5.2]
       t.string :real_name
       t.string :image
       t.string :primary_image
+      t.string :ethnicity 
+      t.string :hometown
+      t.string :current_city
       t.string :social_media
       t.string :instagram
       t.string :twitter
@@ -12,6 +15,7 @@ class CreateQueens < ActiveRecord::Migration[5.2]
       t.string :youtube
       t.string :site
       t.string :imdb
+      t.date :date_of_birth
       t.timestamps
     end
   end
