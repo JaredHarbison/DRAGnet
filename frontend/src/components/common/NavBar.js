68d3c9a8ea47d8785e3fd23c2dd187381a6347e3
Jared Harbison <jared.harbison@gmail.com>
2019-07-30 10:07:16 -0400

    ui inverted fluid fourt item navbar

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = ({dragQueens}) => (
-    <div className="ui menu">
+    <div className="ui inverted fluid four item menu">
 
         <Link className="item" to="/"> DRAGnet </Link>
 
         <Link className="item" to="/queens"> Browse Queens </Link>
 
+        <Link className="item" to="/"> Browse Seasons </Link>
+
+        <Link className="item" to="/"> Your Account </Link>
+
     </div>
 );
 
-export default NavBar 
+export default NavBar
