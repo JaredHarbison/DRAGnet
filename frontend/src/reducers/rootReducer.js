e4f96656da252ddc3cae53da5c7b28959ec805a4
JaredHarbison <jared.harbison@gmail.com>
2019-08-24 20:40:36 -0400

    debug types.js, architect reducers for scale with seasons

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import queens_reducer from './queens_reducer'
 import quotes_reducer from './quotes_reducer'
+import seasons_reducer from './seasons_reducer'
 
 const rootReducer = combineReducers({
     queens: queens_reducer,
-    quotes: quotes_reducer
+    quotes: quotes_reducer, 
+    seasons: seasons_reducer, 
 });
 
 export default rootReducer;
