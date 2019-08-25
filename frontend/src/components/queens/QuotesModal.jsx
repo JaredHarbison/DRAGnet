54e55b8b69002b606818714cfcc9e4ef8c289c26
JaredHarbison <jared.harbison@gmail.com>
2019-08-25 18:08:47 -0400

    add twitter timeline to quotes modal

--- frontend/src/components/queens/QuotesModal.jsx
+++ frontend/src/components/queens/QuotesModal.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Button, Modal } from 'semantic-ui-react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 import CreateQuote from '../quotes/CreateQuote';
 import QueenQuotes from '../quotes/QueenQuotes';
 
 
+
+
 const QuotesModal = ({queen}) => (
     <Modal trigger={<Button size="tiny">{queen.quotes.length} Quotes</Button>} closeIcon>
         <Modal.Content>
-        <img  src={queen.primary_image}
-               alt={queen.drag_name}
-               verticalalign="middle"
-               className="ui small right floated rounded image"/>
+            <div className="ui grid">
+                <div className="ten wide column">
+                    <TwitterTimelineEmbed 
+                        sourceType="timeline"
+                        url={queen.twitter}
+                        theme="dark"
+                        noFooter 
+                        options={{height: 425}}
+                    />
+                </div>
+                <div className="six wide column">
+                    <img  src={queen.primary_image}
+                    alt={queen.drag_name}
+                    verticalalign="middle"
+                    className="ui fluid image"/>
+                </div>
+            </div>
             <CreateQuote queen={queen}/>
             <QueenQuotes queen={queen}/>
         </Modal.Content>
