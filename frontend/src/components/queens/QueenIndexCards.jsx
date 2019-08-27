4b625cd821f3ce51047d5e7614e1229cc695835b
JaredHarbison <jared.harbison@gmail.com>
2019-08-27 17:25:17 -0400

    indentation

--- frontend/src/components/queens/QueenIndexCards.jsx
+++ frontend/src/components/queens/QueenIndexCards.jsx
@@ -15,59 +15,75 @@ const QueenIndexCards = ({ filteredQueens }) => {
                     <img  src={queen.primary_image}
                           alt={queen.drag_name}
                           verticalalign="middle"
-                          className="ui small left floated rounded image"/>
-                    <div className="header">{queen.drag_name}
+                          className= 
+                          "ui small left floated rounded image"/>
+                    <div className="header">
+                        {queen.drag_name}
                     </div>
                     <div className="ui divider"></div>
-                    <div className="header">{ arrayToSentence(queen.seasons.map (season =>
-                        season.season_name)) }</div>
+                    <div className="header">
+                        { 
+                        arrayToSentence(queen.seasons
+                            .map (season =>
+                        season.season_name)) 
+                        }
+                    </div>
                     <div className="ui divider"></div>
                     <span className="header"></span>
                         <div className="header">
                             <a  href={queen.instagram}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="instagram icon"></i></a>
+                                <i className="instagram icon"></i>
+                            </a>
                             <a  href={queen.facebook}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="facebook icon"></i></a>
+                                <i className="facebook icon"></i>
+                            </a>
                             <a  href={queen.twitter}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="twitter icon"></i></a>
+                                <i className="twitter icon"></i>
+                            </a>
                             <a  href={queen.youtube}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="youtube icon"></i></a>
+                                <i className="youtube icon"></i>
+                            </a>
                             <a  href={queen.website}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="home icon"></i></a>
+                                <i className="home icon"></i>
+                            </a>
                             <a  href={queen.imdb}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="imdb icon"></i></a>
+                                <i className="imdb icon"></i>
+                            </a>
                             <a  href={queen.wikipedia}
                                 rel="noopener noreferrer"
                                 target="_blank">
-                                <i className="wikipedia w icon"></i></a></div>
-                            <div className="ui divider"></div>
+                                <i className="wikipedia w icon"></i>
+                            </a>
+                        </div>
+                        <div className="ui divider"></div>
                     <BiographyModal queen={queen}/>
                     <TriviaModal queen={queen}/>
                     <QuotesModal queen={queen}/>
-                    <Link key={queen.id} to={`/queens/${queen.id}`}>
+                    <Link key={queen.id} 
+                          to={`/queens/${queen.id}`}>
                         <Button size="tiny">Edit Info</Button>
                     </Link>
+                    </div>
                 </div>
             </div>
+    );
+    return (
+        <div>
+            {renderQueens}
         </div>
-  );
-  return (
-      <div>
-          {renderQueens}
-      </div>
-  );
+    );
 };
 
 export default QueenIndexCards;
