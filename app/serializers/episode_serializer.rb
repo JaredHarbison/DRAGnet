a320ca84a51f9664a0d5bbbdd9c8f829186caa27
JaredHarbison <jared.harbison@gmail.com>
2019-09-11 20:43:32 -0400

    lend id back to serializer

--- app/serializers/episode_serializer.rb
+++ app/serializers/episode_serializer.rb
@@ -1,5 +1,6 @@
 class EpisodeSerializer < ActiveModel::Serializer
-    attributes :episode_name
+    attributes :episode_name, 
+               :id
     belongs_to :season
     has_many :queens
   end
